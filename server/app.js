e82a33cbfea96a386441e6061f35cf544df81520
Pitoune <pierredheb@gmail.com>
2024-03-22 13:03:00 +0100

    fix Anne code

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     const db = createClient({ url, authToken });
     req.db = db;
